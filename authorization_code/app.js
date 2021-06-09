7fb8a8dfc7d22ebd3663c5265ff2dc0d613d1827
roxywilcox <roxygwilcox@gmail.com>
2021-06-09 16:20:58 -0700

    changed port for heroku

--- authorization_code/app.js
+++ authorization_code/app.js
@@ -1,3 +1,5 @@
+require('dotenv').config()
+
 //import {songs} from '../public/index.html';
 
 /**
@@ -339,4 +341,4 @@ app.get('/refresh_token', function(req, res) {
 
 
 console.log('Listening on 8888');
-app.listen(8888);
+app.listen(process.env.PORT||8888);
